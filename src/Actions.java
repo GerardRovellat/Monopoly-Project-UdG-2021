47e6f8bf0895253b9356e2d19b00fd0de69b29d3
MarcGot <gotcritgmarc@gmail.com>
2021-04-21 16:40:12 +0200

    Creació de les primeres verions de les classes. Unes versions simples i rapides per tenir una guia de la estructura

--- src/Actions.java
+++ src/Actions.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Actions implements Comparable<Actions> {
 
     private Box actual_position;
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+
 public class Actions implements Comparable<Actions> {
-    private String
+    private String Name;
+    private Int money;
+    private ArrayList <Box> boxes_in_property;
+    private int board_position; //??????????
+    //MORE
+
+
 }
