58359bb9ed45359a39b36fce388bb2a61a35c19d
MarcGot <gotcritgmarc@gmail.com>
2021-05-05 19:20:25 +0200

    Classe Bet funcionant i funcio betAction() de monopoly tambe

--- src/Bet.java
+++ src/Bet.java
@@ -1,7 +1,18 @@
+import java.lang.Math;
+
 public class Bet extends Box{
+
+    private int[] combinations = new int[]{0,1,2,3,4,5,6,5,4,3,2,1};
 
     public Bet(int position, String type) {
         super(position, type);
     }
 
+    public int betResult(int quanity, int bet, int dice_result) {
+        if (dice_result >= bet) {
+            Double aux = bet * (1 + Math.pow(10,((36 - combinations[bet-1]) / 36)));
+            return aux.intValue();
+        }
+        else return -1;
+    }
 }
