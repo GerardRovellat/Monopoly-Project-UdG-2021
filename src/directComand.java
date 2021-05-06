26a0c200216b51916fee48c602ffb3d255b35cc9
MarcGot <gotcritgmarc@gmail.com>
2021-05-06 18:28:43 +0200

    Modificació parametres de carta ( eliminació de redundancia de dades )

--- src/directComand.java
+++ src/directComand.java
@@ -1,14 +1,10 @@
 public class directComand extends Box{
     private Card function;
-    private String command;
 
-    public directComand(int position, String type, String command,Card function){
+    public directComand(int position, String type, Card function){
         super(position,type);
         this.function = function;
-        this.command = command;
     }
 
     public Card getCard() { return this.function; }
-
-    public String getCommand() { return this.command; }
 }
